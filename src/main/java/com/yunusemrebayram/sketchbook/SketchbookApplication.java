d48f5076ce0315fffaf11c80b83a0724f3e2091f
ynsbyrm <yunusemre.byrm@gmail.com>
2022-10-05 13:49:40 +0300

    first commit

---
+++ src/main/java/com/yunusemrebayram/sketchbook/SketchbookApplication.java
@@ -0,0 +1,13 @@
+package com.yunusemrebayram.sketchbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SketchbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SketchbookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yunusemrebayram/sketchbook/SketchbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yunusemrebayram.sketchbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SketchbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
